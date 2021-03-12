c60ac24d9746a20e642d20e1600c5bf2b9e8baf1
jihyun-um <ashleyum20@gmail.com>
2021-03-12 10:10:22 +0000

    Add scroll up button to detail pages

--- src/pages/Detail/Layout.js
+++ src/pages/Detail/Layout.js
@@ -1,7 +1,10 @@
-import React, {Component} from "react";
-import {Link, withRouter} from "react-router-dom";
+import React, { Component } from "react";
+import { Link, withRouter } from "react-router-dom";
+import ScrollToTop from "react-scroll-to-top";
 import styled from "styled-components";
 import "../../components/Nav.scss";
+import { ReactComponent as ArrowUpBlack } from "../../styles/arrow_up_black.svg";
+import { ReactComponent as ArrowUpWhite } from "../../styles/arrow_up_white.svg";
 
 const Wrapper = styled.div`
   position: relative;
@@ -38,7 +41,7 @@ const ItemContainer = styled.div`
   @media screen and (min-width: 1024px) {
     min-height: 80vh;
     margin: ${(props) =>
-    props.margin ? props.margin : "70px 160px 30px 160px"};
+      props.margin ? props.margin : "70px 160px 30px 160px"};
 
     &.carrot {
       margin: 0 0 -30px 0;
@@ -53,10 +56,10 @@ const ImageContainer = styled.div`
   margin: 0 auto;
   padding: ${(props) =>
     props.type === "iframe"
-        ? "20px 0 0 0"
-        : props.padding
-        ? props.padding
-        : "20px 32.5%"};
+      ? "20px 0 0 0"
+      : props.padding
+      ? props.padding
+      : "20px 32.5%"};
 
   img {
     width: 100%;
@@ -102,7 +105,7 @@ const IframeContainer = styled.div`
     float: none;
     clear: both;
     text-align: center;
-    
+
     @media screen and (max-width: 1024px) {
       position: absolute;
       top: 0;
@@ -146,8 +149,8 @@ const IframeContainer = styled.div`
     float: none;
     clear: both;
     text-align: center;
-    
-     @media screen and (max-width: 1024px) {
+
+    @media screen and (max-width: 1024px) {
       position: absolute;
       top: 0;
       left: 0;
@@ -183,15 +186,6 @@ const Time = styled.div`
     height: 16px;
   }
 
-  @media screen and (min-width: 1024px) {
-    top: 17px;
-    right: 17px;
-    padding: 5px;
-    font-size: 15px;
-    width: 74px;
-    height: 26px;
-  }
-
   @media screen and (min-width: 1200px) {
     top: 30px;
     left: 30px;
@@ -203,6 +197,37 @@ const Time = styled.div`
   }
 `;
 
+const ScrollToTopButton = styled(ScrollToTop)`
+  position: fixed;
+  z-index: 2;
+  transition: opacity 1s ease-in-out;
+  cursor: pointer;
+  background-color: ${(props) => props.backgroundColor};
+  border-radius: 7px;
+
+  @media screen and (min-width: 320px) {
+    right: 27px;
+    bottom: 17px;
+    width: 25px;
+    height: 25px;
+
+    svg {
+      width: 12px;
+    }
+  }
+
+  @media screen and (min-width: 1200px) {
+    left: 50px;
+    bottom: 30px;
+    width: 40px;
+    height: 40px;
+
+    svg {
+      width: 20px;
+    }
+  }
+`;
+
 const ContentWrapper = styled.div`
   display: flex;
   align-items: center;
@@ -210,8 +235,8 @@ const ContentWrapper = styled.div`
   padding: 20px 0;
   border: ${(props) =>
     props.background === "#000"
-        ? "0.5px solid rgba(255, 255, 255, 0.5)"
-        : "0.5px solid rgba(0, 0, 0, 0.5)"};
+      ? "0.5px solid rgba(255, 255, 255, 0.5)"
+      : "0.5px solid rgba(0, 0, 0, 0.5)"};
   border-left: none;
   border-right: none;
   font-family: "Alliance-Regular" !important;
@@ -236,7 +261,7 @@ const ContentContainer = styled.div`
   font-size: 1.6vmin;
   margin: 0 auto;
   line-height: 1.4;
-  
+
   @media screen and (max-width: 1024px) {
     width: 100vw;
     font-size: 1vmin;
@@ -264,117 +289,127 @@ const BottomContainer = styled.div`
 `;
 
 class Layout extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            date: new Date().toLocaleTimeString("en-US", {hour12: false}),
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: new Date().toLocaleTimeString("en-US", { hour12: false })
+    };
+  }
 
-    tick() {
-        this.setState({
-            date: new Date().toLocaleTimeString("en-US", {hour12: false}),
-        });
-    }
+  tick() {
+    this.setState({
+      date: new Date().toLocaleTimeString("en-US", { hour12: false })
+    });
+  }
 
-    componentDidMount() {
-        this.timerID = setInterval(() => this.tick(), 1000);
-    }
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
 
-    componentWillUnmount() {
-        clearInterval(this.timerID);
-    }
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
 
-    render() {
-
-        return (
-            <Wrapper
-                background={this.props.background}
-                color={this.props.color}
-                id={this.props.id}
+  render() {
+    return (
+      <Wrapper
+        background={this.props.background}
+        color={this.props.color}
+        id={this.props.id}
+      >
+        {window.innerWidth > 1200 ? (
+          <Time
+            className="dropdown"
+            background={this.props.background === "#000" ? "#fff" : "#000"}
+            color={this.props.background === "#000" ? "#000" : "#fff"}
+          >
+            {this.state.date}
+            <div
+              className="dropdown-content"
+              style={{
+                top: "33px",
+                left: "0px",
+                fontFamily: "Alliance-SemiBold"
+              }}
             >
-                {window.innerWidth > 1200 ? (
-                    <Time
-                        className="dropdown"
-                        background={this.props.background === "#000" ? "#fff" : "#000"}
-                        color={this.props.background === "#000" ? "#000" : "#fff"}
-                    >
-                        {this.state.date}
-                        <div
-                            className="dropdown-content"
-                            style={{
-                                top: "33px",
-                                left: "0px",
-                                fontFamily: "Alliance-SemiBold",
-                            }}
-                        >
-                            <Link to="/">Home</Link>
-                            <Link to="/Message">Video: Message</Link>
-                            <Link to="/Carrot">Book: Carrot</Link>
-                            <Link to="/Binary">Poster: Binary</Link>
-                            <Link to="/Toilet">Video: Toilet</Link>
-                            <Link to="/Beauty">Anthology: Beauty</Link>
-                            <Link to="/Love">Poster: Love</Link>
-                        </div>
-                    </Time>
-                ) : (
-                    <Time
-                        background={this.props.background === "#000" ? "#fff" : "#000"}
-                        color={this.props.background === "#000" ? "#000" : "#fff"}
-                        onClick={() => this.props.history.push("/list")}
-                    >
-                        {this.state.date}
-                    </Time>
-                )}
-                <ItemContainer margin={this.props.margin} className={this.props.id}>
-                    {this.props.item.type === "iframe" ? (
-                        <IframeContainer
-                            type={this.props.item.type}
-                            padding={this.props.padding}
-                            minHeight={this.props.id === "message" ? "75vh" : "60vh"}
-                        >
-                            {this.props.item}
-                        </IframeContainer>
-                    ) : (
-                        <ImageContainer
-                            className={this.props.id}
-                            type={this.props.item.type}
-                            padding={this.props.padding}
-                            minHeight={this.props.id === "message" ? "75vh" : "60vh"}
-                        >
-                            {this.props.item}
-                        </ImageContainer>
-                    )}
-                </ItemContainer>
-                <ContentWrapper background={this.props.background}>
-                    <ContentContainer>{this.props.children}</ContentContainer>
-                </ContentWrapper>
-                {!["love", "toilet", "message"].includes(this.props.id) && (
-                    <>
-                        {[...Array(Number(this.props.total)).keys()].map((el, i) => {
-                            return (
-                                <BottomContainer
-                                    key={el}
-                                    padding={
-                                        this.props.id === "beauty" && ![0, 1, 2].includes(i)
-                                            ? "30px 25% 0"
-                                            : this.props.id === "beauty" && i === 2
-                                            ? "30px 37% 0"
-                                            : this.props.bcPadding
-                                    }
-                                >
-                                    <img
-                                        alt={this.props.id}
-                                        src={`https://yjparkworks.s3.amazonaws.com/${this.props.id}/scans/${i}.jpg`}
-                                    />
-                                </BottomContainer>
-                            );
-                        })}
-                    </>
-                )}
-            </Wrapper>
-        );
-    }
+              <Link to="/">Home</Link>
+              <Link to="/Message">Video: Message</Link>
+              <Link to="/Carrot">Book: Carrot</Link>
+              <Link to="/Binary">Poster: Binary</Link>
+              <Link to="/Toilet">Video: Toilet</Link>
+              <Link to="/Beauty">Anthology: Beauty</Link>
+              <Link to="/Love">Poster: Love</Link>
+            </div>
+          </Time>
+        ) : (
+          <Time
+            background={this.props.background === "#000" ? "#fff" : "#000"}
+            color={this.props.background === "#000" ? "#000" : "#fff"}
+            onClick={() => this.props.history.push("/list")}
+          >
+            {this.state.date}
+          </Time>
+        )}
+        <ItemContainer margin={this.props.margin} className={this.props.id}>
+          {this.props.item.type === "iframe" ? (
+            <IframeContainer
+              type={this.props.item.type}
+              padding={this.props.padding}
+              minHeight={this.props.id === "message" ? "75vh" : "60vh"}
+            >
+              {this.props.item}
+            </IframeContainer>
+          ) : (
+            <ImageContainer
+              className={this.props.id}
+              type={this.props.item.type}
+              padding={this.props.padding}
+              minHeight={this.props.id === "message" ? "75vh" : "60vh"}
+            >
+              {this.props.item}
+            </ImageContainer>
+          )}
+        </ItemContainer>
+        <ContentWrapper background={this.props.background}>
+          <ContentContainer>{this.props.children}</ContentContainer>
+        </ContentWrapper>
+        {!["love", "toilet", "message"].includes(this.props.id) && (
+          <>
+            {[...Array(Number(this.props.total)).keys()].map((el, i) => {
+              return (
+                <BottomContainer
+                  key={el}
+                  padding={
+                    this.props.id === "beauty" && ![0, 1, 2].includes(i)
+                      ? "30px 25% 0"
+                      : this.props.id === "beauty" && i === 2
+                      ? "30px 37% 0"
+                      : this.props.bcPadding
+                  }
+                >
+                  <img
+                    alt={this.props.id}
+                    src={`https://yjparkworks.s3.amazonaws.com/${this.props.id}/scans/${i}.jpg`}
+                  />
+                </BottomContainer>
+              );
+            })}
+          </>
+        )}
+        <ScrollToTopButton
+          smooth={true}
+          backgroundColor={this.props.background}
+          component={
+            this.props.background === "#000" ? (
+              <ArrowUpWhite alt="arrow up" />
+            ) : (
+              <ArrowUpBlack alt="arrow up" />
+            )
+          }
+        />
+      </Wrapper>
+    );
+  }
 }
 
 export default withRouter(Layout);
